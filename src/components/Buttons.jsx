422f783a4ba30754e8e86ea38e6d98980c330010
SvyatoyTapok <ewfbewrb@mail.ru>
2023-04-25 01:07:23 +0300

    small changes

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,19 +3,40 @@ import React from "react";
 
 function Buttons(props) {
   const upButtons = ["Обычное тесто", "Тонкое тесто"];
-  const bottomButtons = ["Маленькая", "Большая"];
+  const bottomButtons = ["Маленькая", "Средняя", "Большая"];
+
+  const [activeTypeButton, setActiveTypeButton] = React.useState(0);
+  const [activeSizeButton, setActiveSizeButton] = React.useState(0);
+  const [deletedSmall, setDeletedSmall] = React.useState(false);
+
   return (
     <div className="buttons">
       <div className="type">
-        {upButtons.map((value) => (
-          <div key={value} className="type_buttons clicked">
+        {upButtons.map((value, i) => (
+          <div
+            key={value}
+            onClick={() => {
+              setActiveTypeButton(i);
+              if (i === 1) {
+                console.log("ya nazhal na thin");
+              }
+            }}
+            className={activeTypeButton === i ? "type_buttons clicked" : "type_buttons"}
+          >
             {value}
           </div>
         ))}
       </div>
       <div className="size">
-        {bottomButtons.map((value) => (
-          <div key={value} className="size_buttons clicked">
+        {bottomButtons.map((value, i) => (
+          <div
+            key={value}
+            onClick={() => {
+              setActiveSizeButton(i);
+              setDeletedSmall(true);
+            }}
+            className={activeSizeButton === i ? "size_buttons clicked" : "size_buttons"}
+          >
             {value}
           </div>
         ))}
