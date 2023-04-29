7b69b46bef7432fa4ce36977bb900a3e83b83cc7
SvyatoyTapok <ewfbewrb@mail.ru>
2023-04-30 01:20:36 +0300

    /

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,6 +4,7 @@ import React from "react";
 function Buttons(props) {
   const [activeTypeButton, setActiveTypeButton] = React.useState(0);
   const [activeSizeButton, setActiveSizeButton] = React.useState(0);
+  
 
   if (props.additionalVariants[0].size !== props.mainArray.size) {
     props.additionalVariants.unshift({
@@ -14,9 +15,22 @@ function Buttons(props) {
       type: props.mainArray.type,
     });
   }
+
+  if (props.additionalVariants[0].type !== props.mainArray.type) {
+    props.additionalVariants.unshift({
+      price: props.mainArray.price,
+      weight: props.mainArray.weight,
+      energy: props.mainArray.energy,
+      size: props.mainArray.size,
+      type: props.mainArray.type,
+    });
+  }
+
   let clickedTypeOfSizeArray = props.additionalVariants.filter(
     (obj) => obj.type === props.typeArray[activeTypeButton]
   );
+
+
 
   return (
     <div className="buttons">
@@ -26,18 +40,9 @@ function Buttons(props) {
             className={activeTypeButton === index ? "current_buttons clicked" : "current_buttons"}
             onClick={() => {
               setActiveTypeButton(index);
-              if (index === 0) {
-                setActiveSizeButton(0);
-                props.setCurrentPrice(props.additionalVariants[0].price);
-                props.setCurrentWeight(props.additionalVariants[0].weight);
-                props.setCurrentEnergy(props.additionalVariants[0].energy);
-              }
-              if (index === 1) {
-                setActiveSizeButton(0);
-                props.setCurrentPrice(props.additionalVariants[3].price);
-                props.setCurrentWeight(props.additionalVariants[3].weight);
-                props.setCurrentEnergy(props.additionalVariants[3].energy);
-              }
+              setActiveSizeButton(0);
+
+              
             }}
             key={index}
           >
